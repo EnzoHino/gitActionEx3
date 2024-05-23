d91fc329495d1c0ad3aa78afe19c06878485c16f
EnzoHino <124402091+EnzoHino@users.noreply.github.com>
2024-05-23 09:39:39 -0300

    Update app_teste.py

--- app_teste.py
+++ app_teste.py
@@ -14,7 +14,7 @@ class FlaskAppTests(unittest.TestCase):
   def test_salah(self):
     response = self.client.get('/salah') 
     self.assertEqual(response.status_code, 200) 
-    self.assertIn(b'ok',response.data) 
+    self.assertIn(b'nisfleitopzao',response.data) 
   
   if __name__ == '__main__': 
     unittest.main() 
