88b7d905530b145db8108262525b8aedefa54d1a
EnzoHino <124402091+EnzoHino@users.noreply.github.com>
2024-09-26 22:57:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def devops(): 
-  return '<center><h1><font color=red>Germinare Tech, EU AMO PINTO MÈDIO ROZINHA TROLHA NO ORIFICIO ANAL COMER TOdas pessasos AIAIAIAI MARCOA</center>' 
+  return '<center><h1><font color=blue>Germinare Tech, EU AMO PINTO MÈDIO ROZINHA TROLHA NO ORIFICIO ANAL COMER TOdas pessasos AIAIAIAI MARCOA</center>' 
   
 if __name__ == '__main__': 
   app.run(debug=True, host='0.0.0.0', port=3782) 
